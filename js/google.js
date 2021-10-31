9bdbb2d998dd8f10ff046be2655e5a20535128d9
cavanhaman <cavanhaman@hotmail.com>
2021-10-31 17:40:55 -0300

    everse geocoding Gratuito!

--- js/google.js
+++ js/google.js
@@ -16,43 +16,18 @@ jQuery.get('google.key', function (chaveG) {
 	document.body.appendChild(google_api);
 });
 // =======================================================================
+//FREE REVERSE GEOCODING API
+var reverse_api = document.createElement('script');
+reverse_api.src="https://cdn.jsdelivr.net/gh/bigdatacloudapi/js-reverse-geocode-client@latest/bigdatacloud_reverse_geocode.min.js"
+document.body.appendChild(reverse_api);
 
-/*****************************************************************/
-if (navigator.geolocation)
-	navigator.geolocation.getCurrentPosition(showPosition, showError);
-else
-	x.innerHTML = "Geocoder error!";
-
-function showPosition(position) {
-	latitude = position.coords.latitude;
-	longitude = position.coords.longitude;
-	latlng = [latitude, longitude];
-	console.log("latlng: "+latlng);
-	//codeLatLng()
-	x.innerHTML = "Latitude: " + latitude +
-		"<br>Longitude: " + longitude;
-}
-function showError(error) {
-	x.innerHTML = "Geocoder error!"
-}
-
-const extractCityName = latlng => {
-	googleMapsClient.reverseGeocode({ latlng }, (err, response) => {
-	  if (!err) {
-		return response.json.plus_code.compound_code.split(' ').slice(1).join(' ');
-	  }
-	});
-  }; 
-  // examples: 
-  console.log(extractCityName(40.6599718,-73.9817292));
-  // New York, NY, USA
-  console.log(extractCityName(37.386052, -122.083851));
-  // Mountain View, CA, USA
-  console.log(extractCityName(51.507351, -0.127758));
-  // Westminster, London, UK
-
-
-
+var reverseGeocoder=new BDCReverseGeocode();
+reverseGeocoder.localityLanguage='pt';
+/* Get the administrative location information using a set of known coordinates */
+reverseGeocoder.getClientLocation(function(result) {
+	console.log(result);
+	saida.innerHTML=result.city;      
+});
 /*****************************************************************/
 
 
