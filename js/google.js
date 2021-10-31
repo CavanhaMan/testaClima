64eefb1773ee7ebbc38070154c227b8adb8bda01
cavanhaman <cavanhaman@hotmail.com>
2021-10-31 16:47:27 -0300

    Adicionando testes para reverse geocoding

--- js/google.js
+++ js/google.js
@@ -1,21 +1,60 @@
 var latitude;
 var longitude;
 var chaveG;
-// =================================================
-// PARTE DO GOOGLE:
-function insertGoogleScript() {
-	//A chave do google foi armazenada em um arquivo local 'google.key'
-	//Este arquivo foi incluido no .gitignore para não ser enviado para o github
-	jQuery.get('google.key', function (chaveG) {
-		console.log("chaveG = " + typeof (chaveG));
+var x = document.getElementById("saida");
+var latlng;
 
-		var google_api = document.createElement('script'), api_key = chaveG;
-		// Inject the script for Google's API and reference the initGoogleAPI
-		// function as a callback.
-		google_api.src = 'https://maps.googleapis.com/maps/api/js?key=' + api_key + '&callback=initGoogleAPI&libraries=places,geometry';
-		document.body.appendChild(google_api);
-	});
+// ===PARTE DO GOOGLE:====================================================
+//A chave do google foi armazenada em um arquivo local 'google.key'
+//Este arquivo foi incluido no .gitignore para não ser enviado para o github
+jQuery.get('google.key', function (chaveG) {
+	console.log("chaveG = " + typeof (chaveG));
+	var google_api = document.createElement('script'), api_key = chaveG;
+	// Inject the script for Google's API and reference the initGoogleAPI
+	// function as a callback.
+  	google_api.src = 'https://maps.googleapis.com/maps/api/js?key=' + api_key + '&callback=initGoogleAPI&libraries=places,geometry';
+	document.body.appendChild(google_api);
+});
+// =======================================================================
+
+/*****************************************************************/
+if (navigator.geolocation)
+	navigator.geolocation.getCurrentPosition(showPosition, showError);
+else
+	x.innerHTML = "Geocoder error!";
+
+function showPosition(position) {
+	latitude = position.coords.latitude;
+	longitude = position.coords.longitude;
+	latlng = [latitude, longitude];
+	console.log("latlng: "+latlng);
+	//codeLatLng()
+	x.innerHTML = "Latitude: " + latitude +
+		"<br>Longitude: " + longitude;
 }
+function showError(error) {
+	x.innerHTML = "Geocoder error!"
+}
+
+const extractCityName = latlng => {
+	googleMapsClient.reverseGeocode({ latlng }, (err, response) => {
+	  if (!err) {
+		return response.json.plus_code.compound_code.split(' ').slice(1).join(' ');
+	  }
+	});
+  }; 
+  // examples: 
+  console.log(extractCityName(40.6599718,-73.9817292));
+  // New York, NY, USA
+  console.log(extractCityName(37.386052, -122.083851));
+  // Mountain View, CA, USA
+  console.log(extractCityName(51.507351, -0.127758));
+  // Westminster, London, UK
+
+
+
+/*****************************************************************/
+
 
 // SearchBox Method
 function initGoogleAPI() {
@@ -33,15 +72,19 @@ function initGoogleAPI() {
 	});
 }
 /*
-var x=document.getElementById("saida");
-function showPosition(position){
-	x.innerHTML="Latitude: " + latitude + 
+	$("#saida").innerHTML="
+		Você está aqui ó:
+		<br>Latitude: " + latitude +
+		"<br>Longitude: " + longitude +
+		"<br>Local: " + $('#city-search').val();
+
+	var x=document.getElementById("saida");
+	x.innerHTML="Latitude: " + latitude +
 	"<br>Longitude: " + longitude +
 	"<br>Local: " + $('#city-search').val();
-	}
- */ 
+ */
 
-insertGoogleScript();
+//insertGoogleScript();
 
 //location.href = "tela_clima.html";
 
--- nao/leposicao.js
+++ nao/leposicao.js
@@ -6,29 +6,28 @@
 
 var x=document.getElementById("saida");
 function getLocation(){
-  if (navigator.geolocation){
+  if (navigator.geolocation)
     navigator.geolocation.getCurrentPosition(showPosition,showError);
-    }
-  else{
-    x.innerHTML="Seu navegador não suporta Geolocalização.";}
+  else
+    x.innerHTML="Geolocation is not supported by this browser.";
   }
 function showPosition(position){
   x.innerHTML="Latitude: " + position.coords.latitude + 
-  "<br>Longitude: " + position.coords.longitude;  
-  }
+  "<br>Longitude: " + position.coords.longitude;
+}
 function showError(error){
   switch(error.code) {
     case error.PERMISSION_DENIED:
-      x.innerHTML="Solicitação de Geolocalização negada."
+      x.innerHTML="User denied the request for Geolocation."
       break;
     case error.POSITION_UNAVAILABLE:
-      x.innerHTML="Localização indisponível."
+      x.innerHTML="Location information is unavailable."
       break;
     case error.TIMEOUT:
-      x.innerHTML="A requisição expirou."
+      x.innerHTML="The request to get user location timed out."
       break;
     case error.UNKNOWN_ERROR:
-      x.innerHTML="Erro desconhecido."
+      x.innerHTML="An unknown error occurred."
       break;
     }
   }
